8f4b7c0f1412717eb3ccd79519ae1f02dd9fede3
lauralopez2005 <135733927+lauralopez2005@users.noreply.github.com>
2023-07-27 10:38:16 -0500

    Add files via upload

--- index.php
+++ index.php
@@ -9,46 +9,53 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-4bw+/aepP/YC94hEpVNVgiZdgIC5+VKNBQNGCHeKRQN+PtmoHDEXuppvnDJzQIu9" crossorigin="anonymous">
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-HwwvtgBNo3bZJJLYd8oVXjrBZt8cqVSpeBNS5n7C8IVInixGAoxmnlMuBnhbgrkm" crossorigin="anonymous"></script>
 </head>
-<nav class="navbar navbar-expand-lg bg-body-tertiary">
+<nav class="navbar navbar-expand-lg navbar-dark shadow-5-strong" style= "background-color: #00FF0000;">
     <div class="container-fluid">
       <div class="logo">
         <img src="imagenes/matematicas.png" alt="OPERACIONES MATEMÁTICAS" width="40" height="40" class="d-inline-block align-text-top">
         OPERACIONES MATEMÁTICAS
       </div>
+</div>
 </nav>
-<body>
+<body style= "background-image:url('https://png.pngtree.com/thumb_back/fh260/background/20210911/pngtree-addition-subtraction-multiplication-and-division-letters-puzzle-early-education-cognitive-building-image_854389.jpg');">
 <div class="container text-center">
-    <div class="row">
-      <div class="col">
-        <div class="card" style="width: 18rem;">
-            <img src="imagenes/suma.jpg" class="card-img-top" alt="...">
+    <div class="row" >
+      <div class="col" >
+        <div class="card" style="width: 18rem; background-color: #C1E89F;">
+            <img src="imagenes/suma2.png" class="card-img-top" alt="...">
             <div class="card-body">
               <h5 class="card-title">Suma</h5>
               <a href="suma.php"  class="btn btn-primary">Sumar</a>
             </div>
           </div>
       </div>
-      <div class="container text-center">
-    <div class="row">
       <div class="col">
-        <div class="card" style="width: 18rem;">
-            <img src="imagenes/suma.jpg" class="card-img-top" alt="...">
+      <div class="card" style="width: 18rem; background-color: #C1E89F;">
+            <img src="imagenes/resta.png" class="card-img-top" alt="...">
             <div class="card-body">
               <h5 class="card-title">Resta</h5>
               <a href="resta.php"  class="btn btn-primary">Restar</a>
             </div>
           </div>
       </div>
-      <div class="container text-center">
-    <div class="row">
       <div class="col">
-        <div class="card" style="width: 18rem;">
-            <img src="imagenes/suma.jpg" class="card-img-top" alt="...">
+      <div class="card" style="width: 18rem; background-color: #C1E89F;">
+            <img src="imagenes/division.png" class="card-img-top" alt="...">
             <div class="card-body">
-              <h5 class="card-title">Suma</h5>
-              <a href="suma.php"  class="btn btn-primary">Sumar</a>
+              <h5 class="card-title">División</h5>
+              <a href="division.php"  class="btn btn-primary">Dividir</a>
             </div>
           </div>
       </div>
+      <div class="col">
+      <div class="card" style="width: 18rem; background-color: #C1E89F;">
+            <img src="imagenes/multiplicacion1.png" class="card-img-top" alt="...">
+            <div class="card-body">
+              <h5 class="card-title">Multiplicación</h5>
+              <a href="multiplicacion.php"  class="btn btn-primary">Multiplicar</a>
+            </div>
+          </div>
+      </div>
+</div>
 </body>
 </html>
